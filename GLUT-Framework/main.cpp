cd353675dd54d594dca18fc11e15a5ea7e221212
Tianyu Cheng <tianyu.cheng@utexas.edu>
2014-05-14 18:47:40 -0500

    add OGRE1.9 framework

--- GLUT-Framework/main.cpp
+++ GLUT-Framework/main.cpp
@@ -117,7 +117,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Perspective's GLUT Template");
+    glutCreateWindow("GLUT Template");
 
     // register glut call backs
     glutKeyboardFunc(keyboardDown);
--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Templates
 =========
 
 This is a template repository built for vim-fetch. 
+
+Current Templates
+-----------------
+# Graphics
+## GLUT
+## GLFW
+
+# Game frameworks
+## Ogre 1.9
